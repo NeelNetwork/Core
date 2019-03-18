8cc7497f041a0a1cc80fd8a22ab7a5723541fbd4
farzadhallaji <farzadhallaji@yahoo.com>
2019-03-18 18:54:41 +0330

    simplewallet fix bugs 1

--- pyclient/wallet/simplewallet_cli.py
+++ pyclient/wallet/simplewallet_cli.py
@@ -33,7 +33,7 @@ from wallet.simplewallet_client import SimpleWalletClient
 
 DISTRIBUTION_NAME = 'simplewallet'
 
-DEFAULT_URL = 'http://rest-api:8008'
+DEFAULT_URL = 'tcp://rest-api:8008'
 
 def create_console_handler(verbose_level):
     clog = logging.StreamHandler()
